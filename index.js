b85351f35712e8a4b3d07b1edfbdfb16bf6f4e54
Olekszij <olekszijk@gmail.com>
2024-06-23 23:57:03 +0200

    no comment

--- index.js
+++ index.js
@@ -78,6 +78,6 @@ app.get('*', (req, res) => {
   res.status(404).json({ message: 'Not Found' });
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
 });
